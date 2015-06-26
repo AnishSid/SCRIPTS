f76ccdf7135197a76538f911cc0ea5f4d8253e30
anishsid <anishsidhan@gmail.com>
2015-06-26 02:13:48 -0700

    started my own ohai project

--- Ruby/Sysadmin/machine_stats.rb
+++ Ruby/Sysadmin/machine_stats.rb
@@ -6,11 +6,31 @@ def self.processor_count
 `cat /proc/cpuinfo | grep processor | wc -l`.to_i
 end
 
+def mem_total
+`grep MemTotal /proc/meminfo | awk '{print $2}'`.to_i
+end
+
+
 def memory_usage
  output = %x(free)
 total_memory_usage_in_k = `ps -Ao rss=`.split.map(&:to_i).inject(&:+)
 end
 
-puts "#{memory_usage/1024} USED in Mbytes"
-puts "uptime for your box is #{started}"
-puts "number of CPU's for this machine is #{self.processor_count}"
+def redhat_release
+ if File.exist?("/etc/redhat-release")
+ contents = File.read("/etc/redhat-release").chomp
+ return contents
+ else
+ puts "not a Redhat box"
+ end
+end
+
+def kernel_version 
+ return platform_version = `uname -r`.strip
+end
+
+
+puts "REDHAT VERSION: #{redhat_release}" 
+puts "KERNEL: #{kernel_version}"
+puts "UPTIME: #{started}"
+puts "CPU CORES: #{self.processor_count}"
