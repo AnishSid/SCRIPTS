3ab839f4ed9dc96d5c12460d55d50ef58b937668
anishsid <anishsidhan@gmail.com>
2015-06-27 09:55:53 -0700

    few gems to  machine_stats

--- Ruby/Sysadmin/machine_stats.rb
+++ Ruby/Sysadmin/machine_stats.rb
@@ -1,19 +1,30 @@
+#!/usr/bin/env ruby
+
+=begin
+Load the required Gems
+=end
+
+
+require 'facter'
+require 'sys/filesystem'
+require 'free_disk_space'
+
 def started
      Time.now - IO.read('/proc/uptime').split[0].to_i
 end
 
 def self.processor_count
-`cat /proc/cpuinfo | grep processor | wc -l`.to_i
+     Facter.value('processors')['count']
 end
 
 def mem_total
-`grep MemTotal /proc/meminfo | awk '{print $2}'`.to_i
+    `grep MemTotal /proc/meminfo | awk '{print $2}'`.to_i
 end
 
 
 def memory_usage
- output = %x(free)
-total_memory_usage_in_k = `ps -Ao rss=`.split.map(&:to_i).inject(&:+)
+    output = %x(free)
+    total_memory_usage_in_k = `ps -Ao rss=`.split.map(&:to_i).inject(&:+)
 end
 
 def redhat_release
@@ -29,8 +40,14 @@ def kernel_version
  return platform_version = `uname -r`.strip
 end
 
+def check_disk_space
+FreeDiskSpace.gigabytes('/').to_i
+end
+
 
 puts "REDHAT VERSION: #{redhat_release}" 
 puts "KERNEL: #{kernel_version}"
 puts "UPTIME: #{started}"
 puts "CPU CORES: #{self.processor_count}"
+    puts "kernel: #{Facter.value(:kernel)}"
+puts "DISK USAGE: #{check_disk_space}GB"
