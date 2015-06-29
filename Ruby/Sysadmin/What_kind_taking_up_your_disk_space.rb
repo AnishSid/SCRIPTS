c69a587af5fddd2b5313377ea858271d1e5dc734
anishsid <anishsidhan@gmail.com>
2015-06-28 23:04:24 -0700

    more scripts & changes

---
+++ Ruby/Sysadmin/What_kind_taking_up_your_disk_space.rb
@@ -0,0 +1,20 @@
+puts "Enter the Directory to check: "
+DIRNAME = gets.chomp
+
+hash = Dir.glob("#{DIRNAME}/**/*.*").inject({}) do |hsh, fname|
+     ext = File.basename(fname).split('.')[-1].to_s.downcase
+     hsh[ext] ||= [0,0]
+     hsh[ext][0] += 1
+     hsh[ext][1] += File.size(fname)
+     hsh
+end
+
+File.open("file-analysis.txt", "w") do |f|
+     hash.each do |arr|
+     txt = arr.flatten.join("\t")
+     f.puts txt
+     puts txt
+    end
+end
+     end
+end
