d62ac7e4493b508a5ca815dedc9d13d45a71294b
anishsid <anishsidhan@gmail.com>
2015-07-09 04:00:44 -0700

    small change to  machine_stats

--- Ruby/Sysadmin/machine_stats.rb
+++ Ruby/Sysadmin/machine_stats.rb
@@ -8,6 +8,7 @@ require 'socket'
 require 'facter'
 require 'sys/filesystem'
 require 'free_disk_space'
+require 'system/getifaddrs'
 
 def started
      Time.now - IO.read('/proc/uptime').split[0].to_i
@@ -44,11 +45,28 @@ def check_disk_space
 FreeDiskSpace.gigabytes('/').to_i
 end
 
+def self.list_all_devices()
+      devicehash = System.get_ifaddrs # {:eth0=>{:inet_addr=>"18.248.7.89", :netmask=>"18.248.7.89"}, :vmnet1=>{:inet_addr=>"172.16.76.1", :netmask=>"172.16.76.1"}}
+      devicehash.keys
+end 
+
+def self.list_all_devices_macaddr()
+      devices = self.list_all_devices
+      device_macaddrs = {}
+      devices.each do |eth_device|
+      device_macaddrs[eth_device] = self.mac(eth_device.to_s).unpack('H*')[0]
+      device_macaddrs
+end 
+end
+
+plimit_soft = Process.getrlimit(:NOFILE)[1]
+plimit_hard = Process.getrlimit(:NOFILE)[0]
 
 puts "REDHAT VERSION: #{redhat_release}" 
 puts "KERNEL: #{kernel_version}"
 puts "UPTIME: #{started}"
 puts "CPU CORES: #{self.processor_count}"
-puts "IP's : #{Socket.getifaddrs.map { |i| i.addr.ip_address if i.addr.ipv4? }.compact}"
+puts "NETWORK INTERFACES & IP's :  #{self.list_all_devices} #{Socket.getifaddrs.map { |i| i.addr.ip_address if i.addr.ipv4? }.compact}"
 puts "kernel: #{Facter.value(:kernel)}"
+puts "file descriptor soft_limit : #{plimit_soft} and hard_limit : #{plimit_hard} "
 puts "DISK USAGE: #{check_disk_space}GB"
